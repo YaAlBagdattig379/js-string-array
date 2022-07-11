9bffb1e033ba69bc85cb85290ee3882d80be84a5
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-07-12 00:42:19 +0530

    module 29 first commit

---
+++ case-convert.js
@@ -0,0 +1,45 @@
+const nastionalAnthem = 'amr sonar bangla ami tomai valobasi';
+const searching = 'helo';
+// if(nastionalAnthem ==  )
+
+
+const userInput = 'elePhAnT';
+const userPassword = 'elephant';
+
+// console.log(userPassword.toUpperCase());
+// console.log(userInput.toUpperCase());
+// console.log(userPassword.toLowerCase());
+ if(userInput.toLowerCase() == userPassword.toUpperCase()){
+     console.log('user exists ! you have a chance to go ahead where you want.')
+  }
+ else{
+     console.log('same vabe mil khaina !')
+}
+
+// this is normal eXample of how to convert from "toLowerCase to UpperCase"
+const nationalAnthem = 'amr sonar bangla ami tomai valo basi !!';
+const nationalAntheM = 'Amr sonar bangla ami tomai valo basi!!!';
+if(nationalAntheM.toLowerCase() == nationalAntheM.toUpperCase()){
+    console.log('hello desh basi how are you !')
+}
+else{
+    console.log('kmon kmon mil khailo beta !!!!')
+}
+
+
+
+
+
+// this is example of how to search of string and search 
+
+const propose = 'IloveYoU';
+const propose2You = 'Iloveyou';
+// console.log(propose.toLowerCase());
+// console.log(propose2.toLowerCase());
+
+if(propose.toUpperCase() == propose2You.toLowerCase()){
+    console.log('ok ! we have got a chance for too');
+}
+else{
+    console.log('hello ! we have no words to say all.');
+};
---
+++ string-search.js
@@ -0,0 +1,73 @@
+// example: 1. "How to find out any specific products's "array"  using "indexOf"" 
+const products = [
+    'oppo mobile 12 GB RAM 120 storage',
+    'hp Laptop 12 GB RAM 120 storage',
+    'oppo laptop 12 GB RAM 120 storage',
+    'oppo CellPhone 12 GB RAM 120 storage',
+    'oppo  blue mobile 12 GB RAM 120 storage'
+]
+const searching = 'laptop'
+const outPut = [];
+
+// for(const product of products){
+//     if(product.toLowerCase().indexOf(searching.toLowerCase()) != -1){
+//          outPut.push(product);
+//     }
+// }
+console.log(outPut);
+
+// example: 2."How to find out any specific products of an array using "includes"" 
+// all most 'includes and indexOf same'
+
+const kits = [
+    'water mallon with random shape its size in my pocket ',
+    'coconut joice with random shape its size in my bag ',
+    'water mallon with random shape its size in my bag '  
+]
+const search = 'pocket';
+const outPut2 = [];
+// for(const product of kits){
+//     if(product.toLowerCase().includes(search.toLowerCase())){
+//         outPut2.push(product);
+//     }
+// }
+console.log(outPut2);
+
+
+
+// example: 3."How to find out the products same spelling in fornt word using "startswith"
+
+const color =[
+    ' is a sign of danger in dangeraous red sign',
+    'Red is a tremandous sign of danger ',
+    'is a not a normally symbol red  ',
+    'red is a sign of danger in dangeraous sign'
+]
+const beautufulColor = 'red';
+const outPut3 = [];
+// for(const product of color){
+//     if(product.toLowerCase().startsWith(beautufulColor.toLowerCase())){
+//         outPut3.push(product)
+//     }
+// }
+console.log(outPut3)
+
+
+
+
+
+// example: 4."How to find out the products same spelling in last word using "endswith"
+
+const shopping = [
+    'tomato green  to make foods',
+    'coconut oil for making foods',
+    'to make the frying foods masterd oil is compulsury '
+]
+const sear = 'foods';
+const outPut4 = [];
+for(const product of shopping){
+    if(product.endsWith(sear)){
+        outPut4.push(product);
+    }
+}
+console.log(outPut4);
