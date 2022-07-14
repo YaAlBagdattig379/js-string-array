99875de663ae3df5c680fc434e0549e99c4cc9d1
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-07-14 21:28:29 +0530

    date and time

---
+++ date.js
@@ -0,0 +1,17 @@
+// this is full of "date and time" examples 
+// this is full of "date and time" examples 
+
+// normally two way te date and time dewa jai
+ 
+// 1,,string dia date and time throgh sring way
+const myFevoriteDate = new Date('1947-08-15');
+ console.log(myFevoriteDate);
+
+
+// 2,,,parameter dia date and time set kora hoiche
+const myBirthDay = new Date(1997,07,10,12,45,23);
+console.log(myBirthDay);
+
+
+// this is a website "moment.js eta dia help pawa jai for date and time related"
+// this is a website "moment.js eta dia help pawa jai for date and time related"
