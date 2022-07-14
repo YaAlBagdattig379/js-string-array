5e61625635ce296580f5e3e91828cccd8444c922
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-07-14 21:02:59 +0530

    try-catch

---
+++ try-catch.js
@@ -0,0 +1,16 @@
+// this is in that area where examles of "try catch er"
+
+const nickName = 'mantu'
+const mangoeNames = 'fajali';
+console.log(mangoeNames);
+const subName = 'pintu'
+try{
+    nickName = 'mintu';
+    subName ='palush';
+   
+}catch(error){
+    console.log(' this are infected by catch',error);
+    // console.log(error);
+}
+let nubs = 'what a mass i am not infected by covid-9 !'
+console.log(nubs)
