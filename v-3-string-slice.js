a245f946c9f67b90f5208da4cb58e023ff73120f
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-08-25 23:28:16 +0530

    added something

--- v-3-string-slice.js
+++ v-3-string-slice.js
@@ -3,6 +3,11 @@
 // // const words = anthem.split(' ');
 //   const withouta = anthem.split('a');
 // console.log(withouta);
+// example :: 2 use of "split"
+const anthem22 =  "just you go so fast to do zukarbarg something";
+// const word = anthem22.split('');
+const word = anthem22.split('a');
+// console.log(word);
 
 
 
@@ -12,27 +17,43 @@
 // const anthem2 = 'amr sonar bangla ami tomai valobasi';
 // const words = anthem2.slice(4, 13);
 // console.log(words);
+//  example:: 2.  'slice' 
+const anthem33 = 'amr sonar bangla ami tomai valobasi';
+// const words = anthem33.slice(4, 13);
+// const words = anthem33.slice(7, 17 );
+const words = anthem33.slice(2, 11 );
+// console.log(words);
+
+
 
 
 
 //  example:: 3.  'substr' 
-// const anthem3 = 'amr sonali bangla ami tomai valobasi';
-// const words = anthem3.substr(4, 5);
-// console.log(words);
+const anthem3 = 'amr sonali bangla ami tomai valobasi';
+const word77 = anthem3.substr(4, 5);
+// console.log(word77);
+//  example:: 3..B.  'substr' 
+const anthem31 = 'amr sonali bangla ami tomai valobasi';
+const worldWar2 = anthem31.substr(12, 8);
+// console.log(worldWar2);
+
+
+
 
 
 //  example:: 4.  'substring' 
 // const anthem4 = 'amr sonar bangla ami tomai valobasi';
-// const words = anthem4.substring(4, 8);
-// console.log(words);
+const anthem4 = 'sonar bangla ke ami valobasi';
+const word99 = anthem4.substring(6, 8);
+// console.log(word99);
 
 
 //  example:: 5.  'concat()' 
-// const propose = 'ami';
-// const propose2 = 'Tomai';
-// const propose3 = 'Valobasi';
-// const propose4 = 'Na';
-// const totalWords = propose.concat(propose2).concat(propose3).concat(propose4);
+const propose = 'ami';
+const propose2 = 'Tomai';
+const propose3 = 'Valobasi';
+const propose4 = 'Na';
+const totalWords = propose.concat(propose2).concat(propose3).concat(propose4).concat("hello dosto       2022");
 // console.log(totalWords);
 
 
@@ -51,7 +72,15 @@
 
 
 //  example:: 6-c.  'join()' 
-const propose3 = ['ami','tomai','valo','basi','na'];
+// const propose3 = ['ami','tomai','valo','basi','na'];
 
-const totalWords = propose3.join(' hello ');
-console.log(totalWords);
+// const totalWords = propose3.join(' hello ');
+// console.log(totalWords);
+
+
+//  example:: 6-c.  'join()' 
+// const propose4 = ['ami','tomai','valo','basi','tam'];
+
+// // const totalWords4 = propose4.join(' mon bole ');
+// const totalWords4 = propose4.join(' monbole ');
+// console.log(totalWords4);
