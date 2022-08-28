f6d740a864bec177ed47ed5228d1acc1ced27077
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-08-28 21:57:43 +0530

    git added

--- v-9-date.js
+++ v-9-date.js
@@ -5,13 +5,23 @@
  
 // 1,,string dia date and time throgh sring way
 const myFevoriteDate = new Date('1947-08-15');
- console.log(myFevoriteDate);
+//  console.log(myFevoriteDate);
  //o.p >>>  1947-08-15T00:00:00.000Z
 
 
 // 2,,,parameter dia date and time set kora hoiche
 const myBirthDay = new Date(1997,07,10,12,45,23);
-console.log(myBirthDay); // o.p >>>  1997-08-10T07:15:23.000Z
+// console.log(myBirthDay); // o.p >>>  1997-08-10T07:15:23.000Z
+
+
+// 3 parameter dia date and time set kora hoiche
+const whollyDay = new Date(1907,11,10,5,45,23);
+console.log(whollyDay); // o.p >>>  1997-08-10T07:15:23.000Z
+
+
+// 4 parameter dia date and time set kora hoiche
+const whollyVacation = new Date("1907,11,10,5,45,23");
+console.log(whollyVacation); // o.p >>>  1997-08-10T07:15:23.000Z
 
 
 // this is a website "moment.js eta dia help pawa jai for date and time related"
